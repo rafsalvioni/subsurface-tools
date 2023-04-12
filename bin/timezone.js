cea3386aaf2f419da0a10158fdf38c9dfc267b4a
Rafael M. Salvioni <rafael.salvioni@gmail.com>
2023-04-12 02:34:06 -0300

    For sites, use a public API to get timezones using GPS
    Signed-off-by: Rafael M. Salvioni <rafael.salvioni@gmail.com>

--- bin/timezone.js
+++ bin/timezone.js
@@ -4,16 +4,47 @@ import { download } from '../lib/utils.js';
 function fixTimeZone(input, tz, replace)
 {
     let divelog = new DiveLog(input);
-    let sites   = divelog.getSites();
-    for (const site of sites) {
-        site.setTimeZone(tz, replace);
+    for (const site of divelog.getSites()) {
+        if (!replace && site.getTimeZone()) {
+            continue;
+        }
+        if (site.isLocalized()) {
+            site.setTimeZone(getTimeZoneByGPS(site) ?? tz);
+        }
+        else {
+            site.setTimeZone(tz);
+        }
     }
     for (const dive of divelog) {
+        if (!replace && dive.getTimeZone()) {
+            continue;
+        }
         if (!dive.isLocalized()) {
-            dive.setTimeZone(tz, replace);
+            dive.setTimeZone(tz);
         }
     }
     return divelog.toString();
+}
+
+const TZ_API = 'http://api.geonames.org/timezoneJSON?lat={0}&lng={1}&username=dirkhh';
+
+function getTimeZoneByGPS(site)
+{
+    var xhttp = new XMLHttpRequest();
+    var ret;
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let resp   = JSON.parse(xhttp.responseText);
+            let signal = resp.rawOffset < 0 ? '-' : '+';
+            let tz     = signal + String(Math.abs(resp.rawOffset)).padStart(2, '0') + '00';
+            ret = tz;
+        }
+    };
+    let point = site.getPoint();
+    let url   = TZ_API.format(point.lat, point.lon);
+    xhttp.open("GET", url, false);
+    xhttp.send();
+    return ret;
 }
 
 function process()
@@ -21,7 +52,7 @@ function process()
     let input = document.getElementById('input').value;
     let output;
     try {
-        let tz      = prompt('What TZ do you wanna use?', Date.SYSTEM_TZ);
+        let tz      = prompt('What TZ do you wanna use as default?', Date.SYSTEM_TZ);
         if (!tz) {
             throw 'TZ is required';
         }
